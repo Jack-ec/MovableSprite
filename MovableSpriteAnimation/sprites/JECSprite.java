765577ae1c004e4f5a6669b0f0b1cbb88360b031
904154001 <904154001@L829WD150098.edu.cbe.ab.ca>
2021-04-12 13:00:07 -0600

    workimg

--- MovableSpriteAnimation/sprites/JECSprite.java
+++ MovableSpriteAnimation/sprites/JECSprite.java
@@ -5,15 +5,21 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class JECSprite implements DisplayableSprite, MovableSprite {
-
-	Image image = null;
+	
+	private static Image image;
+	private double centerX = 0;
+	private double centerY = 0;
+	private double width = 50;
+	private double height = 50;
+	private double velocityX = 0;
+	private double velocityY = 0;
 	
 	public JECSprite() {
 		super();
 
 		if (image == null) {
 			try {
-				image = ImageIO.read(new File("res/JEC/JEC-sprite.png"));
+				image = ImageIO.read(new File("res/JEC/sprite.png"));
 			}
 			catch (IOException e) {
 				System.out.println(e.toString());
@@ -23,92 +29,93 @@ public class JECSprite implements DisplayableSprite, MovableSprite {
 
 	@Override
 	public void setCenterX(double centerX) {
-		// TODO Auto-generated method stub
+		this.centerX = centerX;
 		
 	}
 
 	@Override
 	public void setCenterY(double centerY) {
-		// TODO Auto-generated method stub
+		this.centerY = centerY;
 		
 	}
 
 	@Override
 	public void moveX(double pixelsPerSecond) {
-		// TODO Auto-generated method stub
+		this.velocityX = pixelsPerSecond;
 		
 	}
 
 	@Override
 	public void moveY(double pixelsPerSecond) {
-		// TODO Auto-generated method stub
+		this.velocityY = pixelsPerSecond;
 		
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
+		this.velocityX = 0;
+		this.velocityX = 0;
 		
 	}
 
 	@Override
 	public Image getImage() {
 		// TODO Auto-generated method stub
-		return null;
+		return image;
 	}
 
 	@Override
 	public boolean getVisible() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public double getMinX() {
 		// TODO Auto-generated method stub
-		return 0;
+		return centerX - (width / 2);
 	}
 
 	@Override
 	public double getMaxX() {
 		// TODO Auto-generated method stub
-		return 0;
+		return centerX + (width / 2);
 	}
 
 	@Override
 	public double getMinY() {
 		// TODO Auto-generated method stub
-		return 0;
+		return centerY - (width / 2);
 	}
 
 	@Override
 	public double getMaxY() {
 		// TODO Auto-generated method stub
-		return 0;
+		return centerY + (width / 2);
 	}
 
 	@Override
 	public double getHeight() {
 		// TODO Auto-generated method stub
-		return 0;
+		return height;
 	}
 
 	@Override
 	public double getWidth() {
 		// TODO Auto-generated method stub
-		return 0;
+		return width;
 	}
 
 	@Override
 	public double getCenterX() {
 		// TODO Auto-generated method stub
-		return 0;
+		return centerX;
 	}
 
 	@Override
 	public double getCenterY() {
 		// TODO Auto-generated method stub
-		return 0;
+		return centerY;
 	}
 
 	@Override
@@ -119,8 +126,11 @@ public class JECSprite implements DisplayableSprite, MovableSprite {
 
 	@Override
 	public void update(Universe universe, KeyboardInput keyboard, long actual_delta_time) {
-		// TODO Auto-generated method stub
+		double distanceX = this.velocityX * actual_delta_time * 0.001;
+		double distanceY = this.velocityY * actual_delta_time * 0.001;
 		
+		this.centerX += distanceX;
+		this.centerY += distanceY;
 	}
 	
 }
