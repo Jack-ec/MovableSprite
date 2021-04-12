321f0ae1aafc577586ebc499e625798d2f676d5b
jchid <jchid@192.168.1.77>
2021-04-11 19:26:17 -0600

    hjg

--- MovableSpriteAnimation/sprites/JECSprite.java
+++ MovableSpriteAnimation/sprites/JECSprite.java
@@ -20,5 +20,107 @@ public class JECSprite implements DisplayableSprite, MovableSprite {
 			}		
 		}		
 	}
+
+	@Override
+	public void setCenterX(double centerX) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCenterY(double centerY) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveX(double pixelsPerSecond) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveY(double pixelsPerSecond) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getMinX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMaxX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMinY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMaxY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getCenterX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getCenterY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean getDispose() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update(Universe universe, KeyboardInput keyboard, long actual_delta_time) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
